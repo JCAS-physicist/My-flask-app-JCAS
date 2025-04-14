f536841a8c6be926242a8241d6c81d01cad3a4a2
Juan Carlos Avilés Sánchez <78024414+JCAS-physicist@users.noreply.github.com>
2025-04-14 12:41:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, render_template, send_file, make_response
 import subprocess
 import tempfile
 import os
@@ -193,9 +193,10 @@ def index():
         except Exception as e:
             result = f"Error: {str(e)}"
 
-    response = render_template("index.html", result=result, rat=asym,
+    html = render_template("index.html", result=result, rat=asym,
                                S_k_aa=S_k_aa_path, S_k_ca=S_k_ca_path,
                                S_k_cc=S_k_cc_path, select_option=select_option, timestamp=timestamp)
+    response = make_response(html)
     response.cache_control.no_cache = True  # Asegura que no se cacheen
     return response
 
